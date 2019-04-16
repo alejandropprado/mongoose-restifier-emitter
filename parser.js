554b2ace7202cdec83e30ddd99fa1e6569bb337d
Alejandro Prado <alejandropprado@gmail.com>
2019-04-16 11:33:00 -0400

    fix parser

--- parser.js
+++ parser.js
@@ -5,7 +5,13 @@ module.exports = (req, res, next) => {
   let parsed = {}, p = req.query
   try {
     for (var key in p) {
-      if (p.hasOwnProperty(key)) parsed[key] = JSON.parse(p[key])
+      if (p.hasOwnProperty(key)) {
+        try {
+          parsed[key] = JSON.parse(p[key])
+        } catch (error) {
+          parsed[key] = p[key]
+        }
+      }
     }
   }catch(e) {
     next()
